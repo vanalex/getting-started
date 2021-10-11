163101fe6eb94ebf73c0445cb75349cf12901729
alex <alex.pajaron@vier.ai>
2021-10-11 22:53:17 +0200

    quarkus cdi dummy example

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,22 +1,32 @@
 package org.acme.getting.started;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 @Path("/hello")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
+    @Inject
+    Printer printer;
+
     @GET
     public Response hello() {
         return Response.ok(new Greeting("Hello Rest Easy!")).build();
     }
+
+    @GET
+    @Path("/print")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response print() {
+        return Response.ok(new Greeting(printer.print())).build();
+    }
+
 }
